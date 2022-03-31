cf8dc50c5c91f9ae9f679267b47ac012ffd50545
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2022-03-31 00:59:10 -0500

    Coded Cart component + Implement a Dark Mode Switcher using a Button as a Link variant.

--- src/cart.tsx
+++ src/cart.tsx
@@ -1,13 +1,18 @@
-import { VStack, Heading, Text, Button, HStack, AspectRatio, Image, Stack, Divider } from '@chakra-ui/react';
+import { VStack, Heading, Text, 
+Button, HStack, AspectRatio, 
+Image, Stack, Divider, useColorMode, useColorModeValue } from '@chakra-ui/react';
 
 const Cart = () => { 
+    const { toggleColorMode } = useColorMode();
+    const bgColor = useColorModeValue("gray.50", "whiteAlpha.50");
+    const secondaryTextColor = useColorModeValue("gray.600", "gray.400");
     return (
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
+        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg={bgColor}>
             <VStack alignItems="flex-start" spacing={3}>
                 <Heading size="2xl">Your Cart</Heading>
                 <Text>
                     If your price is too hard on your eyes, {''}
-                    <Button variant="link" colorScheme="black">
+                    <Button onClick={toggleColorMode} variant="link" colorScheme="black">
                         try changing the theme.
                     </Button>    
                 </Text>
@@ -20,7 +25,7 @@ const Cart = () => {
                 <Stack spacing={0} w="full" direction="row" justifyContent="space-between" alignItems="center"> 
                     <VStack w="full" spacing={0} alignItems="flex-start">
                         <Heading size="md">Penny Board</Heading>
-                        <Text color="gray.600">PNYCOMP27541</Text>
+                        <Text color={secondaryTextColor}>PNYCOMP27541</Text>
                     </VStack>
                     <Heading size="sm" textAlign="end">$119.00</Heading>
                 </Stack>
@@ -28,15 +33,15 @@ const Cart = () => {
 
             <VStack spacing={4} alignItems="stretch" w="full">
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Subtotal</Text>
+                    <Text color={secondaryTextColor}>Subtotal</Text>
                     <Heading size="sm">$119.00</Heading>
                 </HStack>
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Shipping</Text>
+                    <Text color={secondaryTextColor}>Shipping</Text>
                     <Heading size="sm">$19.99</Heading>
                 </HStack>
                 <HStack justifyContent="space-between">
-                    <Text color="gray.600">Taxes (estimated)</Text>
+                    <Text color={secondaryTextColor}>Taxes (estimated)</Text>
                     <Heading size="sm">$23.80</Heading>
                 </HStack>
             </VStack>
@@ -44,7 +49,7 @@ const Cart = () => {
             <Divider />
 
             <HStack justifyContent="space-between" w="full">
-                <Text color="gray.600">Total</Text>
+                <Text color={secondaryTextColor}>Total</Text>
                 <Heading size="lg">$162.79</Heading>
             </HStack>
         </VStack>
